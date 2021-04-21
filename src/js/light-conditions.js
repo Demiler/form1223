0570a0be7173c4117d1c9e9b6c39625d352cf5df
demiler <seerpout@gmail.com>
2021-04-21 14:09:36 +0300

    Added light condition module

---
+++ src/js/light-conditions.js
@@ -0,0 +1,156 @@
+import { LitElement, html, css } from 'lit-element'
+
+class LightCondition extends LitElement {
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+                padding: 5px;
+                width: 100%;
+                box-sizing: border-box;
+            }
+
+            :host > div {
+            }
+
+            .item {
+                border:none;
+                display: inline-block;
+                padding: 5px;
+                background-color: #d1d1d1;
+                border-radius: 8px;
+                width: 100%;
+                text-align: center;
+                text-transform: uppercase;
+                transition: .2s;
+                cursor: pointer;
+                height: 40px;
+            }
+
+            .item.active {
+                background-color: #cfe6ff;
+            }
+
+            #day {
+                margin-right: 20px;
+            }
+
+            #mean {
+                margin: 0 20px;
+            }
+
+            #daynight {
+                margin-bottom: 10px;
+            }
+
+            #daynight, #values {
+                display: flex;
+                width: 100%;
+                justify-content: space-between;
+                align-items: center;
+                transition: .2s;
+            }
+
+            #values .item:disabled {
+                cursor: not-allowed;
+            }
+        `;
+    }
+
+    static get properties() {
+        return {
+            now: { type: String },
+            value: { type: String },
+        };
+    }
+
+    constructor() {
+        super();
+        this.now = "Day";
+        this.value = "Min";
+    }
+
+    firstUpdated() {
+        this.values = this.shadowRoot.querySelector("#values");
+        this.nowEl = { tar: this.shadowRoot.querySelector("#day") }
+        this.valEl = { tar: this.shadowRoot.querySelector("#min") }
+
+        this.valBtns = [ this.shadowRoot.querySelector("#min"),
+            this.shadowRoot.querySelector("#mean"),
+            this.shadowRoot.querySelector("#max"),
+        ];
+        this.nowEl.tar.classList.add('active');
+        this.valEl.tar.classList.add('active');
+        this.disableButtons();
+    }
+
+    render() {
+        return html`
+            <div id="daynight">
+                <button class='item' id="day" @click=${this.pick}  >Day</button>
+                <button class='item' id="night" @click=${this.pick}>Night</button>
+            </div>
+            <div id="values">
+                <button class='item' id="min" @click=${this.pick} >Min</button>
+                <button class='item' id="mean" @click=${this.pick}>Mean</button>
+                <button class='item' id="max" @click=${this.pick} >Max</button>
+            </div>
+        `;
+    }
+
+    disableButtons() {
+        this.valBtns.forEach(button => button.disabled = true);
+    }
+
+    enableButtons() {
+        this.valBtns.forEach(button => button.disabled = false);
+    }
+
+
+    changeValue(target, newVal) {
+        if (this.now === "NIGHT") {
+            this.value = newVal;
+            this.updateTarget(this.valEl, target);
+        }
+    }
+
+    updateTarget(oldTarget, target) {
+        if (oldTarget.tar === target)
+            return;
+
+        if (oldTarget.tar)
+            oldTarget.tar.classList.remove('active');
+        target.classList.add('active');
+        oldTarget.tar = target;
+    }
+
+    pick(e) {
+        let oldVal = this.val;
+        let oldNow = this.now;
+
+        switch (e.target.innerText) {
+            case "DAY":
+                this.now = "DAY";
+                this.disableButtons();
+                this.updateTarget(this.nowEl, e.target);
+                break;
+            case "NIGHT":
+                this.now = "NIGHT";
+                this.enableButtons();
+                this.updateTarget(this.nowEl, e.target);
+                break;
+            default:
+                this.changeValue(e.target, e.target.innerText);
+        }
+
+        if (oldVal !== this.val || oldNow !== this.now) {
+            let event = new CustomEvent('update', { 
+                detail: { value: this.value },
+                bubbles: true, 
+                composed: true });
+            this.dispatchEvent(event);
+        }
+    }
+}
+
+customElements.define('light-cnd', LightCondition);
