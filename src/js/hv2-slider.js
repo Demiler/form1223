ecf68efb6e46d659416e68607f3382e9fccf6c46
demiler <seerpout@gmail.com>
2021-04-21 21:12:48 +0300

    added slider. A little problem with ticks position

---
+++ src/js/hv2-slider.js
@@ -0,0 +1,27 @@
+import { LitElement, html, css } from 'lit-element'
+
+class HV2Slider extends LitElement {
+    static get styles() {
+        return css`
+        `;
+    }
+
+    static get properties() {
+        return {
+        };
+    }
+
+    constructor() {
+        super();
+    }
+
+    firstUpdated() {
+    }
+
+    render() {
+        return html`
+        `;
+    }
+}
+
+customElements.define('hv2-slider', HV2Slider);
