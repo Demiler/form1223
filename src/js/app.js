c38d472d9be2b31d3c20a22135132f1337e475f9
demiler <seerpout@gmail.com>
2021-04-26 18:12:34 +0300

    Removed old styles from app, changed from single input to fromto input

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ import './ps-input'
 import './light-conditions'
 import './operating-mode'
 import './hv2-slider'
+import './fromto-input'
 
 class Form1223 extends LitElement {
     static get styles() {
@@ -24,6 +25,7 @@ class Form1223 extends LitElement {
                 column-gap: 25px;*/
 
                 width: 800px;
+                font-family: sans;
             }
 
             :host > * {
@@ -47,48 +49,8 @@ class Form1223 extends LitElement {
                 margin-bottom: 5px;
             }
 
-            #geocrd ps-input, #magcrd ps-input {
-                margin-bottom: 5px;
-            }
-
-            #geocrd ps-input, #magcrd ps-input, #LBcrd ps-input {
-                --title-width: 80px;
-                --input-width: 80px;
-            }
-
-
-            #magcrd .title {
-                text-align: end;
-            }
-            #magcrd {
-                display: flex;
-                flex-direction: column;
-                align-items: flex-end;
-            }
-
-            #LBcrd {
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                grid-template-rows: 20px;
-                grid-column: auto / span 2;
-            }
-
-            #LBcrd ps-input.lcrd {
-                justify-self: self-start;
-            }
-
-            #LBcrd ps-input.bcrd {
-                justify-self: self-end;
-            }
-
             #HV2 {
                 grid-column: auto / span 2;
-            }
-
-            .coord-wrap {
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                grid-gap: 10px;
             }
 
             .adc {
@@ -101,14 +63,14 @@ class Form1223 extends LitElement {
     static get properties() {
         return {
             dt:      { type: String },
-            latgeo:  { type: Number },
-            longeo:  { type: Number },
-            altgeo:  { type: Number },
-            latdm:   { type: Number },
-            londm:   { type: Number },
-            r:       { type: Number },
-            l:       { type: Number },
-            b:       { type: Number },
+            latgeo:  { type: Object },
+            longeo:  { type: Object },
+            altgeo:  { type: Object },
+            latdm:   { type: Object },
+            londm:   { type: Object },
+            r:       { type: Object },
+            l:       { type: Object },
+            b:       { type: Object },
             min_hv:  { type: Number },
             max_hv:  { type: Number },
             avg_hv:  { type: Number },
@@ -119,7 +81,14 @@ class Form1223 extends LitElement {
 
     constructor() {
         super();
-        this.latgeo = 478.002502;
+        this.latgeo = { from: 478.002502, to: 1000 };
+        this.longeo = { from: null, to: null };
+        this.altgeo = { from: null, to: null };
+        this.latdm =  { from: null, to: null };
+        this.londm =  { from: null, to: null };
+        this.r =      { from: null, to: null };
+        this.l =      { from: null, to: null };
+        this.b =      { from: null, to: null };
     }
 
     firstUpdated() {
@@ -128,34 +97,32 @@ class Form1223 extends LitElement {
     render() {
         return html`
             <reg-date></reg-date>
-            <div class='coord-wrap'>
-                <div id="geocrd">
-                    <div class='title'>Geographic Registration Coordinates</div>
-                    <ps-input class="latitude"
-                        .value=${this.latgeo}>Latitude:</ps-input>
-                    <ps-input class="longitude"
-                        .value=${this.longeo}>Longitude:</ps-input>
-                    <ps-input class="altitude"
-                        .value=${this.altgeo}>Altitude:</ps-input>
-                </div>
 
-                <div id="magcrd">
-                    <div class='title'>Geomagnetic Event Coordinates</div>
-                    <ps-input class="latitude"
-                        .value=${this.latdm}>Latitude:</ps-input>
-                    <ps-input class="longitude"
-                        .value=${this.londm}>Longitude:</ps-input>
-                    <ps-input class="altitude"
-                        .value=${this.r}>R:</ps-input>
-                </div>
+            <div class='coords' id="geocrd">
+                <div class='title'>Geographic Registration Coordinates</div>
+                <fromto-input class="latitude"
+                    .value=${this.latgeo}>Latitude:</fromto-input>
+                <fromto-input class="longitude"
+                    .value=${this.longeo}>Longitude:</fromto-input>
+                <fromto-input class="altitude"
+                    .value=${this.altgeo}>Altitude:</fromto-input>
+            </div>
 
+            <div class='coords' id="magcrd">
+                <div class='title'>Geomagnetic Event Coordinates</div>
+                <fromto-input class="latitude"
+                    .value=${this.latdm}>Latitude:</fromto-input>
+                <fromto-input class="longitude"
+                    .value=${this.londm}>Longitude:</fromto-input>
+                <fromto-input class="altitude"
+                    .value=${this.r}>R:</fromto-input>
+            </div>
 
-                <div id="LBcrd">
-                    <ps-input class="lcrd"
-                        .value=${this.l}>L-coord:</ps-input>
-                    <ps-input class="bcrd"
-                        .value=${this.b}>B-coord:</ps-input>
-                </div>
+            <div id="LBcrd">
+                <fromto-input class="lcrd"
+                    .value=${this.l}>L-coord:</fromto-input>
+                <fromto-input class="bcrd"
+                    .value=${this.b}>B-coord:</fromto-input>
             </div>
 
             <div>
@@ -164,7 +131,7 @@ class Form1223 extends LitElement {
                     <op-mode></op-mode>
                 </div>
 
-                <ps-input class="adc">Analog-to-digital converter:</ps-input>
+                <fromto-input class="adc">Analog-to-digital converter:</fromto-input>
 
             </div>
 
